d5431f689eab448368a5a7e7b0e7cb0c99581df3
MiroslavKlyukovsky <klyukovskymiroslav@gmail.com>
2024-08-04 20:58:22 +0300

    Fixed strange time shift by stating timezone manually in generate_timestamp().

--- utils.py
+++ utils.py
@@ -1,20 +1,26 @@
 from datetime import datetime
+import pytz
 
 timestamp_format = 'YYYY-MM-DD HH24:MI:SS'
 
 
-def generate_timestamp():
+def generate_timestamp(timezone_str='Europe/London'):
     """
-        Generates a timestamp string in the format 'YYYY-MM-DD HH:MM:SS' based on the current system time.
+        Generates a timestamp string in the format 'YYYY-MM-DD HH:MM:SS' based on the specified timezone.
+        If no timezone is provided, 'Europe/London' is used as the default.
+
+        Parameters:
+        - timezone_str (str): The timezone in which to generate the timestamp. Default is 'Europe/London'.
 
         Returns:
-        - timestamp_str (str): Timestamp string representing the current date and time.
+        - timestamp_str (str): Timestamp string representing the current date and time in the specified timezone.
 
         Raises:
-        - Exception: If an error occurs while generating the timestamp.
+        - Exception: If an error occurs while generating the timestamp or if the timezone is invalid.
     """
     try:
-        current_datetime = datetime.now()
+        timezone = pytz.timezone(timezone_str)
+        current_datetime = datetime.now(timezone)
         date_str = current_datetime.strftime('%Y-%m-%d')
         time_str = current_datetime.strftime('%H:%M:%S')
         timestamp_str = f'{date_str} {time_str}'
